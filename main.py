499d0e05ff9cf20cf438c251390ff6693151ddce
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-07-16 11:36:38 +0530

    model training and prepare callbacks step added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnClassifier import logger
-from cnnClassifier.pipeline.data_ingestion import DataIngestionTrainingPipeline
-from cnnClassifier.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -12,6 +14,9 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+
 STAGE_NAME = "Prepare base model"
 try: 
    logger.info(f"*******************")
@@ -22,3 +27,23 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+
--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -1,22 +1,25 @@
-import os
 import urllib.request as request
 from zipfile import ZipFile
 import tensorflow as tf
 from pathlib import Path
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
+import os
+
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
 
 
+    
     def get_base_model(self):
         self.model = tf.keras.applications.vgg16.VGG16(
-            input_shape = self.config.params_image_size,
-            weights = self.config.params_weights,
-            include_top = self.config.params_include_top
+            input_shape=self.config.params_image_size,
+            weights=self.config.params_weights,
+            include_top=self.config.params_include_top
         )
 
         self.save_model(path=self.config.base_model_path, model=self.model)
+
 
     
     @staticmethod
@@ -64,3 +67,5 @@ class PrepareBaseModel:
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
         model.save(path)
+
+    
